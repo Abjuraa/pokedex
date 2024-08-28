51a5cb95a89abe6893fb2f3f4cf40d2c9e859fc1
Abjura <107774277+Abjuraa@users.noreply.github.com>
2024-08-28 17:28:51 -0500

    Creacion de las rutas de los pokemones, la pokedex y los items

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Pokemons from './pages/Pokemons';
+import Pokemon from './pages/Pokemon';
+import Items from './pages/Items';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/pokemons' element={< Pokemons />} />
+        <Route path='/pokemons/:name' element={< Pokemon />} />
+        <Route path='/items' element={< Items />} />
+        <Route path='/' element={< Pokemons />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export {default as items} from './Items';
+export {default as pokemons} from './Pokemons';
+export {default as pokemon} from './Pokemon';
